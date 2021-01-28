57285c66d6a8b6664e8d5dbbcb87eac8ebacbfdc
Denis Belik <denis.belik2014@gmail.com>
2021-01-28 14:53:30 +0200

    modify NotFound page

--- client/src/views/Errors/NotFound/index.jsx
+++ client/src/views/Errors/NotFound/index.jsx
@@ -1,13 +1,15 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 
-import PageTitle from "../../../components/Misc/PageTitle";
+import sizes from '../../../constants/sizes';
+
+import DefaultLayout from '../../../components/Layouts/Default';
 
 export default function NotFound() {
     return (
-        <Fragment>
-            <PageTitle>Sorry, nothing's here</PageTitle>
-
-            <h2>Sorry, nothing's here</h2>
-        </Fragment>
+        <DefaultLayout title="Sorry, nothing's here">
+            <div className="h-100vh d-flex align-items-center justify-content-center klisi-container" style={{ marginTop: -sizes.navbar }}>
+                <h2>Sorry, nothing's here</h2>
+            </div>
+        </DefaultLayout>
     )
 }
