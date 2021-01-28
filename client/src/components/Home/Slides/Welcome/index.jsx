8411b36b6aeda003f24115bd25dc8336d4b7101f
Denis Belik <denis.belik2014@gmail.com>
2021-01-28 14:18:34 +0200

    make first home slide (welcome slide) look not bad on mobile devices

--- client/src/components/Home/Slides/Welcome/index.jsx
+++ client/src/components/Home/Slides/Welcome/index.jsx
@@ -5,15 +5,17 @@ import HomeEye from '../../HomeEye';
 export default function Welcome() {
     return (
         <section 
-            className="klisi-container d-flex align-items-center justify-content-center"
+            className="klisi-container"
             style={{ height: "100vh" }}
         >
-            <div className="col-6 pl-3 pr-5">
-                <h1>Start conversation with your friends</h1>
-                <p>Klisi will help to video chat with friends/collegues/grandma, just say the word.</p>
-            </div>
-            <div className="pl-5 pr-3">
-                <HomeEye />
+            <div className="d-flex flex-column flex-wrap align-items-center justify-content-center w-100 h-100">
+                <div className="col-lg-6 pl-3 pr-3 pr-lg-5 text-center text-lg-left">
+                    <h1>Start conversation with your friends</h1>
+                    <p>Klisi will help to video chat with friends/collegues/grandma, just say the word.</p>
+                </div>
+                <div className="d-flex align-items-center justify-content-center col-lg-6 pl-5 pr-3 order-first order-lg-last mb-5 mb-lg-0">
+                    <HomeEye />
+                </div>
             </div>
         </section>
     )
