acafa5c3d71a8215d18cd5e1debedac523afe32f
Denis Belik <denis.belik2014@gmail.com>
2021-01-30 18:15:53 +0200

    remove cookie checker

--- client/src/utilities/CookieChecker.js
+++
@@ -1,6 +0,0 @@
-export default class CookieChecker {
-    exists(name) {
-        const regex = new RegExp("^(.*;)?\\s*" + name + "\\s*=\\s*[^;]+(.*)?$");
-        return document.cookie.match(regex);
-    }
-}
--- client/src/utilities/ErrorCatcher.js
+++ client/src/utilities/ErrorCatcher.js
@@ -1,13 +1,8 @@
 export default class ErrorCatcher {
-    call(func, catcher, final) {
-        try { return func(); } 
-        catch (error) { if (catcher) catcher(error); }
-        finally { if (final) final(); }
-    }
-
     async callAsync(func, catcher, final) {
         try { return await func(); } 
         catch (error) { if (catcher) catcher(error); }
         finally { if (final) final(); }
+        return null;
     }
 }
